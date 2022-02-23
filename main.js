193d16d7af95f6b519346c0dd0c0a326bae2e4ec
Akshay0714 <prince.akshayaram@gmail.com>
2022-02-23 13:53:57 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+$.getJSON(
+    "https://api.covid19api.com/total/country/india",
+    function(data){
+        console.log(data);
+
+        var lastElement;
+              
+            for (lastElement in data);
+                lastElement;
+        console.log(lastElement)
+
+        function numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+
+        var total_cases = (data)[lastElement].Confirmed
+        $(".total_cases").append(numberWithCommas(total_cases));
+
+        var total_deaths = data[lastElement].Confirmed
+        $(".total_deaths").append(numberWithCommas(total_deaths));
+    }
+
+)
